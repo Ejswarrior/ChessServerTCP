621fd723648b0d0c1ff809347033d580514e8a92
Erik Sabin <eriksabin97@gmail.com>
2024-04-17 08:02:24 -0400

    Add project files.

---
+++ ChessServerTCP/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using ChessServerTCP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessServerTCP.Controllers
+{
+    [ApiController]
+    [Route("login")]
+    public class AuthController
+    {
+        private _context = 
+        [HttpGet(Name="login")]
+        public Task<ActionResult<User>> createAccount(string userName, string password)
+        {
+
+            _context
+            return userName;
+        }
+    }
+}
---
+++ ChessServerTCP/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ChessServerTCP.Models
+{
+    public class User
+    {
+
+        public string id { get; set; }
+
+        public int totalWins { get; set; }
+
+        public string userName { get; set; }
+
+        public string password { get; set; }
+
+
+    }
+}
