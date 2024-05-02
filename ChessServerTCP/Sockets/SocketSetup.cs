029ee0b2b6db8f210171e8308d6153e645d82000
Erik Sabin <eriksabin97@gmail.com>
2024-05-02 08:20:33 -0400

    feat: got socket connection and recieving a request working

--- ChessServerTCP/Sockets/SocketSetup.cs
+++ ChessServerTCP/Sockets/SocketSetup.cs
@@ -11,31 +11,45 @@ public class SocketSetup
         public async Task<IPEndPoint> getIpEndpoint(string? host = null)
         {
             string hostname = host ?? Dns.GetHostName();
-            IPHostEntry localhost = await Dns.GetHostEntryAsync(hostname);
-            IPEndPoint ipEndPoint = new(localhost.AddressList[0], 11_000);
+        Console.WriteLine(hostname);
+            IPHostEntry localhost = await Dns.GetHostEntryAsync("127.0.0.1");
+        Console.WriteLine("Local address");
+            Console.WriteLine(localhost.AddressList[0].ToString());
+            IPEndPoint ipEndPoint = new(localhost.AddressList[0], 8910);
             return ipEndPoint;
         }
 
         public async void intializeSocket()
         {
             IPEndPoint iPEndPoint = await getIpEndpoint();
+
             using Socket listener = new(
             iPEndPoint.AddressFamily,
             SocketType.Stream,
             ProtocolType.Tcp);
 
+
         Console.WriteLine("Listening on:");
-            Console.WriteLine(iPEndPoint.ToString());
+         Console.WriteLine(iPEndPoint.Address.ToString());
             listener.Bind(iPEndPoint);
-            listener.Listen(100);
+            listener.Listen();
 
-            var handler = await listener.AcceptAsync();
-            while(handler.Connected)
+        Console.WriteLine("before listener");
+         var handler = await listener.AcceptAsync();
+        Console.WriteLine("Before connection");
+
+            while(true)
             {
-                var buffer = new byte[1024];
-                var recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
-                var response = Encoding.UTF8.GetString(buffer, 0, recieved);
+            Console.WriteLine("connection created");
 
+                var buffer = new byte[1024];
+
+            Console.WriteLine(buffer);
+                var recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            Console.WriteLine(recieved.ToString());
+                var response = Encoding.UTF8.GetString(buffer, 0, recieved);
+                
+            Console.WriteLine(response);
                 
                if(response != null)
                 {
